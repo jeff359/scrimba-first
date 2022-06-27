3f7243b76b9e61f16e94a0682e2b4411bdb944cb
Jeffrey Muriithi <jefahk@gmail.com>
2022-06-27 12:39:01 +0300

    new code

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,20 @@
 import React from "react"
+import reactLogo from '../images/react-icon-large.png'
 
 export default function Main() {
     return (
         <main>
-            <h1>Main goes here</h1>
+            <h1 className="main--title">Fun facts about React</h1>
+            <div className="list-img--div">
+                <ul className="main--facts">
+                <li>Was first released in 2007</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+            {/* <img src={reactLogo}alt="react-logo" className="background--logo" /> */}
+            </div>
         </main>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ export default function Navbar() {
     return (
         <nav>
            <img src={reactLogo} alt="react-logo" className="nav--icon" />
-           <h2>ReactFacts</h2>
-           <h3 >React Course - Project 1</h3>
+           <h2 className="nav--logo_text">ReactFacts</h2>
+           <h3 className="nav--title">React Course - Project 1</h3>
         </nav>
     )
 }
